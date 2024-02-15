47297f5fdf65095b42ec1dfb9e5e59bf634a31ba
ronniekram <arynn.boniface@gmail.com>
2024-02-14 21:12:55 -0500

    feat: basic page layout

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 import { sanityIntegration } from "@sanity/astro";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: `http://localhost:4321`,
   integrations: [
+    icon(),
     sanityIntegration({
       projectId: `atx0gy8j`,
       dataset: `production`,
