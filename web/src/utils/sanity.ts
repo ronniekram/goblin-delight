320176fc3df7d81ac27d96c5f2a4ea79488593de
ronniekram <arynn.boniface@gmail.com>
2024-02-17 13:37:07 -0500

    feat: adds all sanity types and their queries

--- web/src/utils/sanity.ts
+++ web/src/utils/sanity.ts
@@ -4,12 +4,145 @@ import type { ImageAsset, Slug } from "@sanity/types";
 import groq from "groq";
 
 //! ----------> QUERIES <----------
+//? ----------> REPEATED QUERIES
+const tags = groq`tags[]->{
+  _type == "reference" => @-> {
+    "ref": {
+      title,
+      slug { current },
+    },
+  },
+}`;
+
+const gameCard = groq`
+  title,
+  slug,
+  description,
+  ${tags},
+  header,
+`;
+
+const gameDetail = groq`*[_type == "game" && slug.current == $slug] {
+  "basic": {
+    title,
+    slug,
+    header,
+    description,
+    features,
+    ${tags},
+  },
+  "details": {
+    release,
+    platforms,
+    genre,
+    madeWith,
+    jam,
+    team,
+    links,
+    blogs[]->{
+      _type == "reference" => @-> {
+        "ref": {
+          title,
+          slug { current },
+        },
+      },
+    },
+  },
+  "media": {
+    videos,
+    images,
+    logoIcons,
+  },
+}`;
+
+const blogCard = groq`
+  title,
+  slug,
+  ${tags},
+  titleImg,
+  _createdAt,
+  "estimatedReadingTime": round(length(pt::text(body)) / 5 / 180 )
+`;
+const blogDetail = groq`*[_type == "post" && slug.current == $slug] {
+  title,
+  slug,
+  titleImg,
+  ${tags},
+  body,
+  related[]-> {
+    _type == "reference" => @-> {
+      "ref": { ${blogCard} }
+    },
+  },
+}`;
+
+const tagDetail = groq`{
+  "blogs": *[_type == "post" && $slug in tags[]-> slug.current] { ${blogCard} },
+  "games": *[_type == "game" && $slug in tags[]-> slug.current] { ${gameCard} },
+}`;
+
+//? ----------> FUNCTIONS
+export const getAllBlogs = async (): Promise<SanityBlogCard[]> => {
+  return await sanityClient.fetch(`*[_type == "post] | order(_createdAt asc){
+    ${blogCard}
+  }`) || [];
+};
+
+export const getBlogDetail = async (options: { slug: string }): Promise<SanityBlog> => {
+  return await sanityClient.fetch(blogDetail, options);
+};
+
+export const getAllGames = async (): Promise<SanityGameCard[]> => {
+  return await sanityClient.fetch(`*[_type == "game"] {
+    ${gameCard}
+  }`) || [];
+};
+
+export const getGameDetail = async (options: { slug: string }): Promise<SanityGame> => {
+  return await sanityClient.fetch(gameDetail, options);
+};
+
+export const getHome = async (): Promise<SanityHome> => {
+  return await sanityClient.fetch(`*[_type == "home"][0]{
+    title,
+    tagline,
+    intro,
+    featured {
+      post {
+        _type == "reference" => @-> { "ref": { ${blogCard} } },
+      },
+      game {
+        _type == "reference" => @-> { "ref": { ${gameCard} } },
+      }
+    }
+  }`);
+};
+
+export const getTagDetails = async (options: { slug: string }): Promise<SanityTagPage> => {
+  return await sanityClient.fetch(tagDetail, options);
+};
+
+export const getPrivacy = async (): Promise<SanityPrivacy> => {
+  return await sanityClient.fetch(`*[_type == "privacy"][0]`);
+};
+
+export const getSocials = async (): Promise<SanitySocials> => {
+  return await sanityClient.fetch(`*[_type == "social"][0]{
+    email,
+    twitch,
+    tiktok,
+    yt,
+    git,
+  }`);
+};
 
 //! ----------> TYPES <----------
 //? ----------> DOCUMENTS
 export type SanityBlog = {
   title: string;
   slug: Slug;
+  _createdAt: string;
+  estTime: number;
   titleImg: ImageAsset;
   tags: SanityTag[];
   body: PortableTextBlock[];
@@ -17,25 +150,34 @@ export type SanityBlog = {
 };
 
 export type SanityGame = {
-  title: string;
-  slug: Slug;
-  header: ImageAsset;
-  description: PortableTextBlock[];
-  features: string[];
-  tags: SanityTag[];
+  basic: {
+    title: string;
+    slug: Slug;
+    header: ImageAsset;
+    description: PortableTextBlock[];
+    features: string[];
+    tags: SanityTag[];
+  };
   // DETAILS
-  team: SanityTeam[];
-  release: string;
-  platforms: string;
-  genre: string;
-  madeWith: string;
-  jam: string;
-  links: SanityLink[];
-  blogs: any[];
+  details: {
+    team: SanityTeam[];
+    release: string;
+    platforms: string;
+    genre: string;
+    madeWith: string;
+    jam: string;
+    links: SanityLink[];
+    blogs: {
+      title: string;
+      slug: string;
+    };
+  };
   // MEDIA
-  videos: SanityYouTube[];
-  images: ImageAsset[];
-  logoIcons: ImageAsset[];
+  media: {
+    videos: SanityYouTube[];
+    images: ImageAsset[];
+    logoIcons: ImageAsset[];
+  };
 };
 
 export type SanityHome = {
@@ -43,13 +185,13 @@ export type SanityHome = {
   tagline: string;
   intro: string;
   featured?: {
-    blog?: SanityBlog[];
-    game?: SanityGame;
+    blog?: SanityBlogCard;
+    game?: SanityGameCard;
   };
 };
 
 export type SanityPrivacy = {
-  title: string;
+  _updatedAt: string;
   body: PortableTextBlock[];
 };
 
@@ -67,6 +209,23 @@ export type SanityTag = {
 };
 
 //? ----------> OBJECTS & HELPERS
+export type SanityBlogCard = {
+  title: string;
+  slug: Slug;
+  tags: SanityTag[];
+  titleImg: ImageAsset;
+  _createdAt: string;
+  estimatedReadingTime: number;
+};
+
+export type SanityGameCard = {
+  title: string;
+  slug: Slug;
+  description: PortableTextBlock[];
+  tags: SanityTag[];
+  header: ImageAsset;
+};
+
 export type SanityGallery = {
   images: ImageAsset[];
   display: `single` | `two` | `carousel` | `inline`;
@@ -80,6 +239,11 @@ export type SanityGameDetail = {
 export type SanityLink = {
   label: string;
   url: string;
+};
+
+export type SanityTagPage = {
+  games: SanityGameCard[];
+  blogs: SanityBlogCard[];
 };
 
 export type SanityTeam = {
