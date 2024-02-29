640d8b9c28a4c4a9f6829387e6537c6732a24fbf
ronniekram <arynn.boniface@gmail.com>
2024-02-28 20:40:19 -0500

    feat: sitemap link update, add sitemap to head

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { sanityIntegration } from "@sanity/astro";
